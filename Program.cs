9240c8d03981bb6ff0b79423033e85cb8d4aa66c
sevensay <120299542+sevensay@users.noreply.github.com>
2023-02-16 18:06:40 +0600

    Fourth commit

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
     for (int i = 0; i < size; i++)
     {
         Console.Write($"Введите {i}-й элемент массива: ");
-        newStringArray[i] = Console.ReadLine();
+        newStringArray[i] = Console.ReadLine()!;
         //Console.WriteLine();
     }
 
