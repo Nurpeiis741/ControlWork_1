5b90cd8ac3f5f268c9b042bbfbd5abb513ccf0ba
sevensay <120299542+sevensay@users.noreply.github.com>
2023-02-16 18:00:31 +0600

    second commit

--- Program.cs
+++ Program.cs
@@ -11,3 +11,38 @@
 
     return newStringArray;
 }
+void PrintArray(string[] arrayToPrint)
+{
+    Console.Write("[ ");
+
+    for (int i = 0; i < arrayToPrint.Length - 1; i++)
+    {
+        Console.Write("\"" + arrayToPrint[i] +"\"" + " , ");
+    }
+
+    Console.Write("\"" + arrayToPrint[arrayToPrint.Length - 1] + "\"" + " ]");
+    Console.WriteLine();
+}
+
+string[] SelectThreeSymbolString(string[] stringArray)
+{
+    int size = stringArray.Length;
+    int newSize = 0;
+    int k = 0;
+    string[] newStringArray = new string[newSize];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            newSize++;
+            Array.Resize(ref newStringArray, newSize);
+            newStringArray[k] = stringArray[i];
+            k++;
+
+        }
+    }
+
+
+    return newStringArray;
+}
